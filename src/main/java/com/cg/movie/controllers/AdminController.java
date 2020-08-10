4b0ab6ed9c50ff68a7e284fa71dae9a35689fd12
krishna agarwal <35657404+krishnaagarwalyh@users.noreply.github.com>
2020-08-10 10:00:16 +0530

    City test & theatre controller updated

--- src/main/java/com/cg/movie/controllers/AdminController.java
+++ src/main/java/com/cg/movie/controllers/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -143,6 +144,13 @@ public class AdminController {
 	{
 		List<Theatre> theatre=theatreService.viewAllTheatre();
 		return new ResponseEntity<List<Theatre>>(theatre,HttpStatus.OK);
+	}
+	
+	@PutMapping("/theatre/edit/{theatreId}")
+	public ResponseEntity<String> updateTheatre(@RequestBody Theatre theatre, @PathVariable long theatreId)
+	{
+		theatreService.updateTheatre(theatre);
+		return new ResponseEntity<String>("Theatre updated successfully", HttpStatus.OK);
 	}
 	
 	@PostMapping(value="/city")
