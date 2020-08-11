9b950ff099b06eaf0515aec32541c9c75ac98bff
krishna agarwal <35657404+krishnaagarwalyh@users.noreply.github.com>
2020-08-11 13:37:01 +0530

    theatre & city service comment updated

--- src/main/java/com/cg/movie/services/CityServiceImpl.java
+++ src/main/java/com/cg/movie/services/CityServiceImpl.java
@@ -3,22 +3,35 @@ package com.cg.movie.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.movie.dao.CityRepository;
 import com.cg.movie.entities.City;
 import com.cg.movie.entities.Theatre;
+import com.cg.movie.exception.CityNotFoundException;
+import com.cg.movie.exception.TheatreNotFoundException;
 @Service
 public class CityServiceImpl implements ICityService {
 
 	@Autowired
 	CityRepository cityRepo;
 	
+	private Logger logger = Logger.getLogger(getClass());
+	
+	/*
+	 * Function to Add city
+	 */
+	
 	@Override
 	public City addCity(City city) {
 		return cityRepo.save(city);
 	}
+	
+	/*
+	 * Function to Get All Cities
+	 */
 
 	@Override
 	public List<City> viewAllCity() {
@@ -26,15 +39,37 @@ public class CityServiceImpl implements ICityService {
 		return cities;
 	}
 
+	/*
+	 * Function to Get all theatre in a particular city
+	 */
+	
 	@Override
 	public List<Theatre> getAllTheatreByCity(String city) {
 		// TODO Auto-generated method stub
 		City requiredCity= cityRepo.findByCityName(city);
-		int size=requiredCity.getTheatresList().size();
-	    List<Theatre> theatreList = new ArrayList<Theatre>(size); 
-	    for (Theatre theatre : requiredCity.getTheatresList()) 
-	      theatreList.add(theatre);
-	    return theatreList;
+		if(requiredCity==null)
+		{
+			logger.error("City Not Found");
+			throw new CityNotFoundException("City Not Exist");
+		}
+		else
+		{
+			logger.info("City Found Successfully");
+			int size=requiredCity.getTheatresList().size();
+		    
+		    if(size==0)
+		    {
+		    	logger.error("Theatre Not Found For "+ city);
+		    	throw new TheatreNotFoundException("Theatre Not Found");
+		    }
+		    else
+		    {
+		    	List<Theatre> theatreList = new ArrayList<Theatre>(size);
+			    for (Theatre theatre : requiredCity.getTheatresList()) 
+				      theatreList.add(theatre);
+				    return theatreList;
+		    }
+		}
 	}
 
 }
